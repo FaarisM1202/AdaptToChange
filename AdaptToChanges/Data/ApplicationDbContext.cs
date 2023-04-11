873053a0a894c94a07a856acd31a270cc9325ef0
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-04-11 12:45:53 -0700

    Set the DB for Members

--- AdaptToChanges/Data/ApplicationDbContext.cs
+++ AdaptToChanges/Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AdaptToChanges.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdaptToChanges.Data
 {
-    public class AdaptToChangesDbContext : IdentityDbContext
+    public class AdaptToChangesDbContext : DbContext
     {
         public AdaptToChangesDbContext(DbContextOptions<AdaptToChangesDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Member> Members { get; set; }
     }
 }
