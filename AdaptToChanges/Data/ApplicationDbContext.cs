bee11f771194a7985c22707402ffcbf65de3a64d
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-04-11 15:18:13 -0700

    Added identity to the project.

--- AdaptToChanges/Data/ApplicationDbContext.cs
+++ AdaptToChanges/Data/ApplicationDbContext.cs
@@ -1,15 +1,15 @@
 ﻿using AdaptToChanges.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace AdaptToChanges.Data
 {
-    public class AdaptToChangesDbContext : DbContext
+    public class AdaptToChangesDbContext : IdentityDbContext
     {
         public AdaptToChangesDbContext(DbContextOptions<AdaptToChangesDbContext> options)
             : base(options)
         {
         }
-
-        public DbSet<Member> Members { get; set; }
     }
 }
