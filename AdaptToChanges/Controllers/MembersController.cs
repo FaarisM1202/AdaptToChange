d1765bdb0946a5f34092160037e5077e67a7939a
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-04-13 18:03:11 -0700

    Added a Members controller and class.

---
+++ AdaptToChanges/Controllers/MembersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdaptToChanges.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ AdaptToChanges/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AdaptToChanges.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+        [Required]
+        public string MemberName { get; set; }
+       
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+
+
+}
