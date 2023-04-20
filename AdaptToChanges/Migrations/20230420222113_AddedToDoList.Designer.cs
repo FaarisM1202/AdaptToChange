634976b86f455fb1126e574e3ecc11ac02f79700
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-04-20 15:24:26 -0700

    Added an AddToDoList migration.

--- AdaptToChanges/Migrations/20230420222113_AddedToDoList.Designer.cs
+++ AdaptToChanges/Migrations/20230420222113_AddedToDoList.Designer.cs
@@ -4,6 +4,7 @@ using AdaptToChanges.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AdaptToChange.Migrations
 {
     [DbContext(typeof(AdaptToChangesDbContext))]
-    partial class AdaptToChangesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230420222113_AddedToDoList")]
+    partial class AddedToDoList
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
